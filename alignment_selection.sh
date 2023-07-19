b68166eb8e1319dc94e192e1ab4828100c4e48f4
Gaurav Sablok <sablokg@gmail.com>
2023-07-19 14:23:15 +0530

    Update alignment_selection.sh

--- alignment_selection.sh
+++ alignment_selection.sh
@@ -1,6 +1,6 @@
 PS3 = "Genomic alignment of the PacBio and the Illumina reads (#)? "; \ 
 select choice in blasr lra_sam lra_paf lra_bed S-conLSH_paf S-conLSH_sam bowtie star; do
-if [[ $choice = bowtie ]]
+if [[ $choice == bowtie ]]
    then echo "bowtie2-build $genome $genome \
         paste Illumina.R1.txt Illumina.R2.txt \
                         | while read col1 col2; \
@@ -9,7 +9,7 @@ if [[ $choice = bowtie ]]
                 -1 ${col1} -2 ${col2} -S $genome.sam \
                     --no-unal --al-conc $genome.aligned.fastq"; break; fi
           
-if [[ $choice = star ]]
+if [[ $choice == star ]]
     then echo "STAR --runThreadN 6 \
                      --runMode genomeGenerate \
                      --genomeDir $genome_dir \
@@ -23,24 +23,24 @@ if [[ $choice = star ]]
                           --outSAMtype BAM SortedByCoordinate \
                             --outSAMunmapped Within \
                               --outSAMattributes Standard"; break; fi                 
-if [[ $choice = blasr ]]
+if [[ $choice == blasr ]]
    then echo "blasr $reads $genome --bam \
          $genome.bam --unaligned $genome.unaligned.fasta \
               --nproc $threads"; break; fi
-if [[ $choice = lra_sam ]]
+if [[ $choice == lra_sam ]]
    then echo " lra index -CCS/CLR/ONT/CONTIG $reference \
          lra align --CCS/CLR/ONT/CONTIG $reference $reads \
              -t $threads -p -s > $reference.sam"; break; fi
-if [[ $choice = lra_paf ]]
+if [[ $choice == lra_paf ]]
    then echo " lra index -CCS/CLR/ONT/CONTIG $reference \
          lra align --CCS/CLR/ONT/CONTIG $reference $reads \
              -t $threads -p -p > $reference.paf"; break; fi
-if [[ $choice = lra_bed ]]
+if [[ $choice == lra_bed ]]
    then echo "lra index -CCS/CLR/ONT/CONTIG $reference \
          lra align --CCS/CLR/ONT/CONTIG $reference $reads \
              -t $threads -p -b > $reference.bed"; break; fi
-if [[ $choice = S-conLSH_paf ]]
+if [[ $choice == S-conLSH_paf ]]
     then echo " S-conLSH $reference $reads > $reference.paf"; break; fi
-if [[ $choice = S-conLSH_sam ]]
+if [[ $choice == S-conLSH_sam ]]
      then echo " S-conLSH $reference $reads --align 1 > $reference.sam"; break; fi
      done
